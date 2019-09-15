8587bfeb8366bec60cf9264f130514d756876e1f
Tianshi Li <9413fi@gmail.com>
2019-09-15 19:18:37 -0400

    Part 2

--- main.js
+++ main.js
@@ -1,3 +1,10 @@
 function createHelloWorldAlert() {
   alert("Hello world!");
 }
+
+function makeInstructorNamesDisappear() {
+  document.getElementById("jason").setAttribute("style", "display: none")
+  document.getElementById("tianshi").setAttribute("style", "display: none")
+  document.getElementById("sai").setAttribute("style", "display: none")
+  document.getElementById("michael").setAttribute("style", "display: none")
+}
